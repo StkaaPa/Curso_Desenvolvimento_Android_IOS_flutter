f99bda00216ea0af5ca952b2f13920a28cd38a29
Samuel gonçalves <goncalvesamuel99@gmail.com>
2022-03-10 19:27:25 +0000

    Update main.dart

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -4,11 +4,27 @@ void main(){
 
   runApp(MaterialApp(
     title: "Frases do Dia",
-    home: Row(children: <Widget>[
-      Text("t1"),
-      Text("t2")
-    ],),
-    //home: Container(color: Colors.white,),
-  ));
+    home: Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Text(
+        "Lorem ipsum",
+          style: TextStyle(
+              fontSize: 25,
+            color: Colors.black,
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.normal,
+            letterSpacing: 0,
+            wordSpacing: 20,
+            decoration: TextDecoration.underline,
+              decorationColor: Colors.black,
+            decorationStyle: TextDecorationStyle.solid
+            ),
+          ),
+        ],),
+      ),
+    ),
+  );
 
 }
